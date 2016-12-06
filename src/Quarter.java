074023a25f9867ae43f40831bc7759a075fb58b9
Justin Watts <justinhwatts9@gmail.com>
2016-12-06 17:41:15 -0500

    Add files via upload

---
+++ src/Quarter.java
@@ -0,0 +1,16 @@
+/**
+ * Quarter.java - Contains the specific details of a quarter.
+ *
+ * @author Justin Watts
+ * @version 10/1/15
+ */
+public class Quarter extends Coin
+{
+	/**
+	 * Constructor of the Quarter class. Sets the name and amount for a quarter.
+	 */
+	public Quarter()
+	{
+		super("Quarter", .25);
+	}
+}
